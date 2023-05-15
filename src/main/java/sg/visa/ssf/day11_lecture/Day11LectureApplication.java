8816ac3631c5752eaa0b946b720981e1c48c601a
jylmarcus <maplecow@gmail.com>
2023-05-15 11:37:30 +0800

    Initial commit

---
+++ src/main/java/sg/visa/ssf/day11_lecture/Day11LectureApplication.java
@@ -0,0 +1,14 @@
+package sg.visa.ssf.day11_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Day11LectureApplication.class);
+		app.run(args);
+	}
+
+}
---
+++ src/test/java/sg/visa/ssf/day11_lecture/Day11LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.visa.ssf.day11_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
